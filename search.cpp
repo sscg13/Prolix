5c46ba8641cd73a0007be5f128f35786de19d15f
sscg13 <iamdumbybear1@gmail.com>
2025-04-28 09:49:57 +0800

    do less lmp when not improving
    bench: 1800932

--- search.cpp
+++ search.cpp
@@ -258,7 +258,7 @@ int Engine::alphabeta(int depth, int ply, int alpha, int beta, int color,
     int mov = Bitboards.moves[ply][i];
     if (!iscapture(mov)) {
       quiets++;
-      if (quiets > depth * depth + depth + 1) {
+      if (quiets > (depth + 2 - improving) * depth + 1) {
         prune = true;
       }
       r = std::min(depth - 1, lmr_reductions[depth][quiets]);
