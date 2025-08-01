a69b86bd377bdc15a995aa0336fb5a8d3a7d4a67
sscg13 <iamdumbybear1@gmail.com>
2025-08-01 08:02:51 +0800

    Claude tweak
    bench: 4031404

--- src/search.cpp
+++ src/search.cpp
@@ -259,7 +259,7 @@ int Engine::alphabeta(int depth, int ply, int alpha, int beta, int color,
     int mov = Bitboards.moves[ply][i];
     if (!iscapture(mov)) {
       quiets++;
-      if (quiets > depth * depth + depth + !improving) {
+      if (quiets > depth * depth + depth + 2 * !improving) {
         prune = true;
       }
       r = std::min(depth - 1, lmr_reductions[depth][quiets]);
