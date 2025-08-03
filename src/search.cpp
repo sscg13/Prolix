dab32d8a7d3caf997b01596e96f8d2c868f992bc
sscg13 <iamdumbybear1@gmail.com>
2025-08-03 12:16:14 +0800

    take n+1
    bench: 5681497

--- src/search.cpp
+++ src/search.cpp
@@ -259,7 +259,7 @@ int Engine::alphabeta(int depth, int ply, int alpha, int beta, int color,
     int mov = Bitboards.moves[ply][i];
     if (!iscapture(mov)) {
       quiets++;
-      if (i > depth * depth + depth + 4 * improving) {
+      if (i > depth * depth + improving * depth + 4) {
         prune = true;
       }
       r = std::min(depth - 1, lmr_reductions[depth][quiets]);
