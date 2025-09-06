8150cdf8613f21b57cc252f387bbc3b87c5ed73e
sscg13 <iamdumbybear1@gmail.com>
2025-09-06 10:53:07 -0700

    reduce more in nmp when staticeval is much larger than beta
    bench: 3945714

--- src/search.cpp
+++ src/search.cpp
@@ -204,7 +204,8 @@ int Engine::alphabeta(int depth, int ply, int alpha, int beta, int color,
     Bitboards.makenullmove();
     searchstack[ply].playedmove = 0;
     int childnodetype = EXPECTED_PV_NODE ? EXPECTED_ALL_NODE : 3 - nodetype;
-    score = -alphabeta(std::max(0, depth - 2 - (depth + 1) / 3), ply + 1, -beta,
+    int r = 2 + (depth + 1) / 3 + std::min(2, (staticeval - beta) / 209);
+    score = -alphabeta(std::max(0, depth - r), ply + 1, -beta,
                        1 - beta, color ^ 1, false, childnodetype);
     Bitboards.unmakenullmove();
     if (score >= beta) {
