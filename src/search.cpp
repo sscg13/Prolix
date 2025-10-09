5473776d835b345f0afc901d3b54726e0c0cff48
sscg13 <iamdumbybear1@gmail.com>
2025-10-08 22:48:12 -0700

    fix nmp node type determination
    bench: 5092779

--- src/search.cpp
+++ src/search.cpp
@@ -249,7 +249,7 @@ int Searcher::alphabeta(int depth, int ply, int alpha, int beta, bool nmp,
       (staticeval >= beta && !isPV) && (!tthit || ttnmpgood)) {
     Bitboards.makenullmove();
     searchstack[ply].playedmove = 0;
-    int childnodetype = EXPECTED_PV_NODE ? EXPECTED_ALL_NODE : 3 - nodetype;
+    int childnodetype = (nodetype == EXPECTED_PV_NODE) ? EXPECTED_ALL_NODE : 3 - nodetype;
     score = -alphabeta(std::max(0, depth - 2 - (depth + 1) / 3), ply + 1, -beta,
                        1 - beta, false, childnodetype);
     Bitboards.unmakenullmove();
