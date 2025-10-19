36f6eec973dc1bb2266230eb6a49132f36e28f88
sscg13 <iamdumbybear1@gmail.com>
2025-10-19 12:19:00 -0700

    less lmr when in check
    bench: 4971218

--- src/search.cpp
+++ src/search.cpp
@@ -359,6 +359,7 @@ int Searcher::alphabeta(int depth, int ply, int alpha, int beta, bool nmp,
     }
     r -= 1024 * isPV;
     r -= 1024 * improving;
+    r -= 512 * incheck;
     r += 512 * (nodetype == EXPECTED_CUT_NODE);
     r = std::max(0, r);
     if (nullwindow && !incheck && !prune && depth < 6) {
