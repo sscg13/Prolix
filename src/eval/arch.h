fc590d048ea42468e03aa47c47fc12acea143921
Chris Bao <89361988+sscg13@users.noreply.github.com>
2025-12-05 18:12:21 -0800

    Working scalar multilayer (#69)
    bench: 3161101

--- src/eval/arch.h
+++ src/eval/arch.h
@@ -5,26 +5,27 @@ constexpr bool mirrored = true;
 constexpr int mirrordivisor = mirrored ? 2 : 1;
 constexpr int realbuckets = inputbuckets / mirrordivisor;
 constexpr int L1size = 768;
-constexpr int L2size = 8;
+constexpr int L2size = 16;
 
 constexpr int L3size = 32;
 constexpr int outputbuckets = 8;
 constexpr int evalscale = 400;
-constexpr int L1bits = 8;
-constexpr int L1Q = (1 << L1bits) - 1;
-constexpr int L3Q = 64;
+constexpr int L1Qbits = 8;
+constexpr int L1Q = (1 << L1Qbits) - 1;
+constexpr int L3Qbits = 6;
+constexpr int L3Q = (1 << L3Qbits);
 constexpr int L4Q = 64;
 constexpr int pairwiseshiftbits = 9;
 // clang-format off
 constexpr int kingbuckets[64] = {
-  0, 0, 0, 0, 1, 1, 1, 1,
-  2, 2, 2, 2, 3, 3, 3, 3,
-  4, 4, 4, 4, 5, 5, 5, 5,
-  4, 4, 4, 4, 5, 5, 5, 5,
-  6, 6, 6, 6, 7, 7, 7, 7,
-  6, 6, 6, 6, 7, 7, 7, 7,
-  6, 6, 6, 6, 7, 7, 7, 7,
-  6, 6, 6, 6, 7, 7, 7, 7
+   0,  0,  0,  0,  1,  1,  1,  1,
+   2,  2,  2,  2,  3,  3,  3,  3,
+   4,  4,  4,  4,  5,  5,  5,  5,
+   4,  4,  4,  4,  5,  5,  5,  5,
+   6,  6,  6,  6,  7,  7,  7,  7,
+   6,  6,  6,  6,  7,  7,  7,  7,
+   6,  6,  6,  6,  7,  7,  7,  7,
+   6,  6,  6,  6,  7,  7,  7,  7
 };
 // clang-format on
 constexpr int material[6] = {1, 1, 1, 1, 1, 0};
@@ -32,11 +33,25 @@ constexpr int bucketdivisor = 32 / outputbuckets;
 // #define MULTI_LAYER
 #ifdef MULTI_LAYER
 constexpr bool multilayer = true;
-constexpr int L2Q = 128;
+constexpr int L2Qbits = 7;
 // #define DUAL_ACTIVATION
 #else
 constexpr bool multilayer = false;
-constexpr int L2Q = 64;
+constexpr int L2Qbits = 6;
 // #define SINGLE_LAYER_CRELU
+#endif
+constexpr int L2Q = (1 << L2Qbits);
+#ifdef DUAL_ACTIVATION
+constexpr int L2shiftbits = 2 * L1Qbits - pairwiseshiftbits + L2Qbits - L3Qbits;
+constexpr int activatedL2size = L2size * 2;
+constexpr int totalL2Q = L3Q;
+constexpr int totalL3Q = L3Q * L3Q * L3Q;
+constexpr int totalL4Q = totalL3Q * L4Q;
+#else
+constexpr int L2shiftbits = 0;
+constexpr int activatedL2size = L2size;
+constexpr int totalL2Q = ((L1Q * L1Q) >> pairwiseshiftbits) * L2Q;
+constexpr int totalL3Q = totalL2Q * L3Q;
+constexpr int totalL4Q = totalL3Q * L4Q;
 #endif
 // #define THREAT_INPUTS
