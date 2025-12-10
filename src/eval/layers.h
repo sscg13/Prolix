17dcd587e4566aad01d45b8e39d8a926b5f220cf
sscg13 <iamdumbybear1@gmail.com>
2025-12-09 21:21:05 -0800

    avx2 affine
    bench: 2158000

--- src/eval/layers.h
+++ src/eval/layers.h
@@ -1,100 +1,13 @@
 #include "../consts.h"
+#include "affine.h"
 #include "arch.h"
 #include <cmath>
-#include <immintrin.h>
-#include <string.h>
 #pragma once
 
 template <typename T> T crelu(T x, T Q);
 I32 csqr(I32 x, I32 Q);
 
-template <int inputsize, int outputsize> struct SparseAffineWeights {
-  alignas(64) I8 weights[outputbuckets * inputsize * outputsize];
-  alignas(64) I32 bias[outputbuckets * outputsize];
-  static constexpr int size = outputbuckets * outputsize * (inputsize + 4);
-  void load(const char *stream) {
-    if (!__builtin_cpu_supports("avxvnni")) {
-      load_scalar(stream);
-      return;
-    }
-    int offset = 0;
-    for (int b = 0; b < outputbuckets; b++) {
-      for (int k = 0; k < inputsize; k += 4) {
-        for (int j = 0; j < outputsize; j++) {
-          for (int i = 0; i < 4; i++) {
-            int dest_idx = outputsize * k + 4 * j + i;
-            int src_idx = j * inputsize + (k + i);
-            weights[offset + dest_idx] = stream[offset + src_idx];
-          }
-        }
-      }
-      offset += inputsize * outputsize;
-    }
-    memcpy(bias, stream + offset, 4 * outputbuckets * outputsize);
-  }
-  void load_scalar(const char *stream) {
-    int offset = 0;
-    memcpy(weights, stream + offset, outputbuckets * inputsize * outputsize);
-    offset += outputbuckets * inputsize * outputsize;
-    memcpy(bias, stream + offset, 4 * outputbuckets * outputsize);
-  }
-};
 
-template <int inputsize, int outputsize> struct SparseAffine {
-  static void
-  transform(const U8 *input, I32 *output,
-            const SparseAffineWeights<inputsize, outputsize> *weights,
-            int bucket) {
-    if (!__builtin_cpu_supports("avxvnni")) {
-      transform_scalar(input, output, weights, bucket);
-      return;
-    }
-    for (int i = 0; i < outputsize; i++) {
-      output[i] = weights->bias[bucket * outputsize + i];
-    }
-    int offset = bucket * inputsize * outputsize;
-    const __m256i *weightptr = (const __m256i *)(&(weights->weights[offset]));
-    // Use two accumulators to break dependency chains
-    __m256i outvec1 = _mm256_loadu_si256((__m256i *)output);
-    __m256i outvec2 = _mm256_setzero_si256();
-
-    // Process 8 steps of K per iteration (2 blocks of 4)
-    for (int k = 0; k < inputsize / 4; k += 2) {
-      // --- Block 1 (k) ---
-      __m256i w1 = _mm256_load_si256((__m256i *)(weightptr + k));
-      __m256i in1 = _mm256_set1_epi32(*(const int32_t *)&input[4 * k]);
-      outvec1 = _mm256_dpbusd_epi32(outvec1, in1, w1);
-
-      // --- Block 2 (k+1) ---
-      // This can execute while Block 1 is still calculating!
-      __m256i w2 = _mm256_load_si256((__m256i *)(weightptr + k + 1));
-      __m256i in2 = _mm256_set1_epi32(*(const int32_t *)&input[4 * (k + 1)]);
-      outvec2 = _mm256_dpbusd_epi32(outvec2, in2, w2);
-    }
-
-    // Sum the two accumulators at the end
-    outvec1 = _mm256_add_epi32(outvec1, outvec2);
-    _mm256_storeu_si256((__m256i *)output, outvec1);
-  }
-  static void
-  transform_scalar(const U8 *input, I32 *output,
-                   const SparseAffineWeights<inputsize, outputsize> *weights,
-                   int bucket) {
-    for (int i = 0; i < outputsize; i++) {
-      output[i] = weights->bias[bucket * outputsize + i];
-    }
-    for (int i = 0; i < inputsize; i++) {
-      if (input[i]) {
-        const I8 *vector =
-            &(weights->weights[bucket * inputsize * outputsize + i]);
-        const int factor = input[i];
-        for (int j = 0; j < outputsize; j++) {
-          output[j] += vector[j * inputsize] * factor;
-        }
-      }
-    }
-  }
-};
 
 template <int inputsize> struct VectorAdd {
   static void transform(const I32 *input, I32 *output) {
