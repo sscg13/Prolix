7dcaaa2e51983f015e365353ee94786ccafc2872
sscg13 <iamdumbybear1@gmail.com>
2025-12-31 11:41:25 -0800

    baseline smallnet
    bench: 2928677

--- src/eval/arch.h
+++ src/eval/arch.h
@@ -1,12 +1,12 @@
 #pragma once
 
-constexpr int inputbuckets = 12;
+constexpr int inputbuckets = 2;
 constexpr bool mirrored = true;
 constexpr bool pairwise = true;
 constexpr bool perspectivecrelu = false;
 constexpr int mirrordivisor = mirrored ? 2 : 1;
 constexpr int realbuckets = inputbuckets / mirrordivisor;
-constexpr int L1size = 768;
+constexpr int L1size = 256;
 constexpr int activatedL1size = L1size * (2 - pairwise);
 constexpr int L2size = 8;
 constexpr int L3size = 32;
@@ -22,14 +22,14 @@ constexpr int L4Q = 64;
 constexpr int l1shiftbits = 9;
 // clang-format off
 constexpr int kingbuckets[64] = {
-   0,  0,  2,  2,  3,  3,  1,  1,
-   4,  4,  6,  6,  7,  7,  5,  5,
-   8,  8,  8,  8,  9,  9,  9,  9,
-   8,  8,  8,  8,  9,  9,  9,  9,
-  10, 10, 10, 10, 11, 11, 11, 11,
-  10, 10, 10, 10, 11, 11, 11, 11,
-  10, 10, 10, 10, 11, 11, 11, 11,
-  10, 10, 10, 10, 11, 11, 11, 11
+   0,  0,  0,  0,  1,  1,  1,  1,
+   0,  0,  0,  0,  1,  1,  1,  1,
+   0,  0,  0,  0,  1,  1,  1,  1,
+   0,  0,  0,  0,  1,  1,  1,  1,
+   0,  0,  0,  0,  1,  1,  1,  1,
+   0,  0,  0,  0,  1,  1,  1,  1,
+   0,  0,  0,  0,  1,  1,  1,  1,
+   0,  0,  0,  0,  1,  1,  1,  1
 };
 // clang-format on
 constexpr int material[6] = {1, 1, 1, 1, 1, 0};
