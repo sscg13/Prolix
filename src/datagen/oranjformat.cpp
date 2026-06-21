cef1e1b618e88544021f088c4a473abdb5ff9df9
Chris Bao <89361988+sscg13@users.noreply.github.com>
2026-06-21 13:39:49 -0700

    Oranjformat datagen (#93)
    bench: 5149935

---
+++ src/datagen/oranjformat.cpp
@@ -0,0 +1,42 @@
+#include "oranjformat.h"
+
+PackedBoard::PackedBoard(const Board &Bitboards, int eval)
+    : occupancy(Bitboards.Bitboards[0] | Bitboards.Bitboards[1]), pieces{},
+      stm((Bitboards.position & 1) << 7), halfmove(Bitboards.position >> 1),
+      fullmove(0), score((I16)eval), wdl(0), padding(0) {
+  U64 occupied = occupancy;
+  int i = 0;
+  while (occupied) {
+    int square = __builtin_ctzll(occupied);
+    pieces[i / 2] |= (((U8)Bitboards.pieces[square]) << (4 * (i & 1)));
+    occupied &= (occupied - 1);
+    i++;
+  }
+}
+
+MoveScorePair::MoveScorePair(int mov, int eval)
+    : move(mov & 4095), score((I16)eval) {
+  if (mov & (1 << 20)) {
+    move |= 0x1000;
+  }
+}
+
+void Oranjformat::initialize(const Board &Bitboards, int eval) {
+  initialpos = PackedBoard(Bitboards, eval);
+  moves.clear();
+}
+
+void Oranjformat::push(int move, int score) {
+  moves.emplace_back(move, score);
+}
+
+void Oranjformat::writewithwdl(std::ofstream &output, int wdl) {
+  initialpos.wdl = wdl;
+  moves.emplace_back(0, 0);
+  output.write(reinterpret_cast<const char *>(&initialpos), 32);
+  output.write(reinterpret_cast<const char *>(moves.data()), 4 * moves.size());
+}
+
+int Oranjformat::positioncount() {
+  return moves.size() - 1;
+}
---
+++ src/datagen/oranjformat.h
@@ -0,0 +1,32 @@
+#include "../board.h"
+#pragma once
+struct [[gnu::packed]] PackedBoard {
+  U64 occupancy;
+  U8 pieces[16];
+  U8 stm;
+  U8 halfmove;
+  U16 fullmove;
+  I16 score;
+  U8 wdl;
+  U8 padding;
+  PackedBoard() = default;
+  PackedBoard(const Board &Bitboards, int eval);
+};
+
+struct [[gnu::packed]] MoveScorePair {
+  U16 move;
+  I16 score;
+  MoveScorePair() = default;
+  MoveScorePair(int mov, int eval);
+};
+
+class Oranjformat {
+  PackedBoard initialpos;
+  std::vector<MoveScorePair> moves;
+
+public:
+  void initialize(const Board &Bitboards, int eval);
+  void push(int move, int score);
+  void writewithwdl(std::ofstream &output, int wdl);
+  int positioncount(); // meant to be called after writewithwdl
+};
